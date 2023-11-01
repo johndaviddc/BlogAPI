c9dd895ed8d747ae2582f41f8e3a139e68e49fb0
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-01 11:14:40 +0800

    added model package

---
+++ src/main/java/dave/dev/blogapi/model/Post.java
@@ -0,0 +1,93 @@
+package dave.dev.blogapi.model;
+
+import jakarta.persistence.*;
+
+import javax.xml.crypto.Data;
+import javax.xml.stream.events.Comment;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date creationDate;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments;
+
+    public Post() {
+    }
+
+    public Post(Long id, String title, String content, String author, Date creationDate, List<Comment> comments) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.creationDate = creationDate;
+        this.comments = comments;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+}
