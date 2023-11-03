d20a5b5e9ed6f8f8a05e21fd7778403d4585a8d8
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-03 16:31:37 +0800

    added post service

--- src/main/java/dave/dev/blogapi/service/PostService.java
+++ src/main/java/dave/dev/blogapi/service/PostService.java
@@ -1,8 +1,12 @@
 package dave.dev.blogapi.service;
 
+import dave.dev.blogapi.model.Post;
 import dave.dev.blogapi.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -13,5 +17,32 @@ public class PostService {
         this.postRepository = postRepository;
     }
 
-    
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public Optional<Post> getPostById(Long postId) {
+        return postRepository.findById(postId);
+    }
+
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    public Post updatePost(Long postId, Post updatedPost) {
+        if (postRepository.existsById(postId)) {
+            updatedPost.setId(postId);
+            return postRepository.save(updatedPost);
+        } else {
+            throw new ResourceNotFoundException("Post with ID " + postId + " not found.");
+        }
+    }
+
+    public void deletePost(Long postId) {
+        if (postRepository.existsById(postId)) {
+            postRepository.deleteById(postId);
+        } else {
+            throw new ResourceNotFoundException("Post with ID " + postId + " not found.");
+        }
+    }
 }
