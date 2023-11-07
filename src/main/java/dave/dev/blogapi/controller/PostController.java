0bfe1b4fa346dad27f896dcaea2384271475f293
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-07 14:31:17 +0800

    added controller

---
+++ src/main/java/dave/dev/blogapi/controller/PostController.java
@@ -0,0 +1,26 @@
+package dave.dev.blogapi.controller;
+
+import dave.dev.blogapi.model.Post;
+import dave.dev.blogapi.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+}
