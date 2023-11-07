6aaa99735682729dd2fccf4bc54edea98bc42e1b
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-07 14:33:44 +0800

    added get mapping

--- src/main/java/dave/dev/blogapi/controller/PostController.java
+++ src/main/java/dave/dev/blogapi/controller/PostController.java
@@ -1,9 +1,11 @@
 package dave.dev.blogapi.controller;
 
+import dave.dev.blogapi.exception.ResourceNotFoundException;
 import dave.dev.blogapi.model.Post;
 import dave.dev.blogapi.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,11 @@ public class PostController {
     @GetMapping
     public List<Post> getAllPosts() {
         return postService.getAllPosts();
+    }
+
+    @GetMapping("/{postId}")
+    public Post getPostById(@PathVariable Long postId) {
+        return postService.getPostById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with ID " + postId + " not found."));
     }
 }
